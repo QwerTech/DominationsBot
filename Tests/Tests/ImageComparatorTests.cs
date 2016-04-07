3bbd4fd87157fc46f8bdafc92472975e9b543b03
QwerTech <novikovpavel90@gmail.com>
2016-04-07 14:55:39 +0300

    for build

--- Tests/Tests/ImageComparatorTests.cs
+++ Tests/Tests/ImageComparatorTests.cs
@@ -1,10 +1,8 @@
-﻿using System.Diagnostics;
-using System.Drawing;
+﻿using System.Drawing;
 using DominationsBot.DI;
 using DominationsBot.Services.ImageProcessing.ImageComporators;
 using NUnit.Framework;
 using StructureMap;
-using Stopwatch = NUnit.Framework.Compatibility.Stopwatch;
 
 namespace Tests.Tests
 {
@@ -13,15 +11,15 @@ namespace Tests.Tests
     {
         private readonly IContainer _container = new Container(new RootRegistry());
 
-        private static readonly object[] Images =
+        private static readonly TestCaseData[] Images =
         {
-            new object[] { TestScreens.Battle_1, TestScreens.Battle_2, true},
-            new object[] { TestScreens.Food_1, TestScreens.Food_2, false},
-            new object[] { TestScreens.sshot_1, TestScreens.sshot_2, false},
-            new object[] { TestScreens.Store_1, TestScreens.Store_2, true},
-            new object[] { TestScreens.screen_1, TestScreens.screen_2, true},
-            new object[] { TestScreens.simple_1, TestScreens.simple_2, true},
-            new object[] { TestScreens.space_1, TestScreens.space_2, false}
+            new TestCaseData(TestScreens.Battle_1, TestScreens.Battle_2, true) {TestName = "Battle"},
+            new TestCaseData(TestScreens.Food_1, TestScreens.Food_2, false) {TestName = "Food"},
+            new TestCaseData(TestScreens.sshot_1, TestScreens.sshot_2, false) {TestName = "sshot"},
+            new TestCaseData(TestScreens.Store_1, TestScreens.Store_2, true) {TestName = "Store"},
+            new TestCaseData(TestScreens.screen_1, TestScreens.screen_2, true) {TestName = "screen"},
+            new TestCaseData(TestScreens.simple_1, TestScreens.simple_2, false) {TestName = "simple"},
+            new TestCaseData(TestScreens.space_1, TestScreens.space_2, false) {TestName = "space"}
         };
 
         [Test]
@@ -29,7 +27,7 @@ namespace Tests.Tests
         public void Compare(Bitmap one, Bitmap another, bool result)
         {
             var actual = _container.GetInstance<ByteLevelComparer>().Compare(one, another);
-            Assert.That(actual,Is.EqualTo(result));
+            Assert.That(actual, Is.EqualTo(result));
         }
     }
 }
